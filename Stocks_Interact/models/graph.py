7df99397e20ec160605b478c1ebad2172ed24a60
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-10-28 20:26:52 +0200

    graph module

--- Stocks_Interact/models/graph.py
+++ Stocks_Interact/models/graph.py
@@ -6,39 +6,39 @@ import requests
 from config import settings
 from models.data import AlphaVantageAPI
 import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 
 
-av=AlphaVantageAPI()
+av = AlphaVantageAPI()
 
 
 class GraphBuilder:
     def __init__(self):
         pass
-    
-    def close_graph(self, ticker, n_observations = None):
+
+    def close_graph(self, ticker, n_observations=None):
+        """time series graph about closing prices"""
         df = av.get_daily(ticker)
-        if n_observations == None:
-            new_df = df
-        elif n_observations <= len(df):
-            new_df = df.iloc[:n_observations,:]
+        if n_observations <= len(df):
+            new_df = df.iloc[:n_observations, :]
         else:
-            raise Exception(f"Invalid number of days")
-        
-        fig = px.line(new_df, x=new_df.index, y=new_df['close'], title=f"Closing Price of {ticker}")
-    
+            new_df = df
+
+        fig = px.line(new_df, x=new_df.index, y=new_df['close'],
+                      title=f"Closing Price of {ticker}")
+
         return fig
-    
-    def volume_graph(self, ticker, n_observations = None):
-        """time series graph about """
+
+    def volume_graph(self, ticker, n_observations=None):
+        """time series graph about Sales Volume"""
         df = av.get_daily(ticker)
-        if n_observations == None:
-            new_df = df
-        elif n_observations <= len(df):
-            new_df = df.iloc[:n_observations,:]
+        if n_observations <= len(df):
+            new_df = df.iloc[:n_observations, :]
         else:
-            raise Exception(f"Invalid number of days")
-        
-        fig = px.line(new_df, x=new_df.index, y=new_df['volume'], title=f"Sales Volume for {ticker}")
+            new_df = df
+
+        fig = px.line(new_df, x=new_df.index, y=new_df['volume'],
+                      title=f"Sales Volume for {ticker}")
 
         return fig
